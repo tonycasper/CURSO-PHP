4c654daf6bec1c28d6e64f088e97bf1c7bcf9af9
php 03 <php.03@metralha.cec.ime.usp.br>
2017-02-08 22:26:32 -0200

    finalzim da aula

---
+++ Arrays/TestaArray.php
@@ -0,0 +1,11 @@
+<?php
+
+$cursos = array("cursodephp","python","mtematica");
+
+$cursos[] ="Logica de programacao";
+
+foreach ($cursos as $x => $valor){
+  echo "Chave: $x, Valor: $valor".PHP_EOL;
+}
+
+ ?>
